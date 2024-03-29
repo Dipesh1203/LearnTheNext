300b91dea9d9bad10d90dd00f959f447f363532e
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-03-30 02:59:08 +0530

    login signup

---
+++ public/Hackathon/main/index.js
@@ -0,0 +1,133 @@
+const st1 = "We are the creators";
+const st2 = "We are the representators";
+const st3 = "We are the devlopers";
+
+const textgenerator = document.getElementsByClassName('.text');
+
+for(let i = 0; i>0 ;i++ ){
+    var t = document.getElementsById("text").textContent;
+    // if(i%1 == 0) {
+        var y = document.createTextNode("This just got added");
+        t.appendChild(y);
+    // }
+    
+    
+
+}
+
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink= document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+});
+btnPopup.addEventListener('click', ()=>{
+    wrapper.classList.add('active-popup');
+    textgenerator.computedStyleMap.display = 'none';
+});
+iconClose.addEventListener('click', ()=>{
+    wrapper.classList.remove('active-popup');
+});
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+// (() => {
+//     'use strict'
+  
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     const forms = document.querySelectorAll('.validate')
+  
+//     // Loop over them and prevent submission
+//     Array.from(forms).forEach(form => {
+//       form.addEventListener('submit', event => {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         }
+  
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+//   })()
+
+
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
